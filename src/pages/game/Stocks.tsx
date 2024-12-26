87d59511fb9a8735032b031bc1e463f07eb04f37
David Budziwojski <dsbudziwojski@gmail.com>
2024-12-25 21:14:26 -0800

    created table, implementing/testing FetchStockPrices function

--- src/pages/game/Stocks.tsx
+++ src/pages/game/Stocks.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
-import {NavLink} from "react-router-dom";
+import React, {useEffect} from 'react'
+import {Link, NavLink} from "react-router-dom";
+
+interface Stock {
+  name: string;
+  ticker: string;
+  id: number
+}
 
 function Stocks() {
+    const Options: Array<Stock> = [
+      {name: 'Apple Inc.', ticker: 'AAPL', id: 1},
+      {name: 'Nvidia Corporation', ticker: 'NVDA', id: 2},
+      {name: 'Microsoft Corporation', ticker: 'MSFT', id: 3},
+      {name: 'Amazon.com, Inc', ticker: 'AMZN', id: 4},
+      {name: 'Alphabet Inc.', ticker: 'GOOGL', id: 5},
+      {name: 'Meta Platforms, Inc.', ticker: 'META', id: 6},
+      {name: 'Tesla, Inc', ticker: 'TSLA', id: 7}
+    ]
+
+        console.log(FetchStockPrices(Options))
   return(
       <>
         <NavLink to='/dashboard'><button>Dashboard</button></NavLink>
@@ -9,8 +26,40 @@ function Stocks() {
         <NavLink to='/portfolio'><button>Portfolio</button></NavLink>
         <NavLink to='/'><button>Give Up</button></NavLink>
         <div>Stocks</div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Ticker</th>
+              <th>Current Price</th>
+            </tr>
+          </thead>
+          <tbody>
+          {Options.map((stock) => {
+            return(
+                <tr key={stock.id}>
+                  <td>{stock.name}</td>
+                  <td>{stock.ticker}</td>
+                  <td>{}</td>
+                </tr>
+            )
+          })}
+          </tbody>
+        </table>
       </>
   )
+}
+
+function FetchStockPrices(Stocks: Array<Stock>) {
+  let StockPrices: Array<number> = []
+  for(let i = 0; i < Stocks.length; i++){
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${Stocks[i].ticker}&interval=5min&apikey=${process.env.REACT_APP_ALPHA_ADVANTAGE_API_KEY}`)
+        .then(response => response.json())
+        .then(json => console.log(json))t
+        .catch(error => console.error(error))
+  }
+
+  return(0)
 }
 
 export default Stocks
