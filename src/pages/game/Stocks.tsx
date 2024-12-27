a0b42465d8390bb30cab727a24399701b41e01a9
David Budziwojski <dsbudziwojski@gmail.com>
2024-12-26 16:37:48 -0800

    changed fetching method, created pagination file, added bootstrap

--- src/pages/game/Stocks.tsx
+++ src/pages/game/Stocks.tsx
@@ -1,27 +1,51 @@
 import React, {useEffect, useState} from 'react'
-import {Link, NavLink} from "react-router-dom";
+import {NavLink} from "react-router-dom";
+import './Stocks.css';
 
 interface Stock {
   name: string;
   ticker: string;
-  id: number
+  currency: string;
+  market: string;
+  exchange: string;
+  id: string;
 }
 
 function Stocks() {
-  const [options, setOptions] = useState<Array<Stock>>()
+  const [options, setOptions] = useState<Array<Stock>>();
+  const [loading, setLoading] = useState(false);
+  const [currentPageNum, setCurrentPageNum] = useState(0);
+  const [stocksPerPage, setStocksPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(`https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&limit=${stocksPerPage}&apiKey=${process.env.REACT_APP_POLYGONIO_API_KEY}`)
+  const [history, setHistory] = useState<Array<string>>([`https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&limit=${stocksPerPage}&apiKey=${process.env.REACT_APP_POLYGONIO_API_KEY}`])
+
 
   useEffect(() => {
-    fetch(`https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit=1000&sort=market&apiKey=${process.env.REACT_APP_POLYGONIO_API_KEY}`)
-      .then(response => response.json())
-      .then(json => {
-        let temp: Array<Stock> = [];
-        for(let i = 0; i < 1000; i++){
-          temp.push({name: json.results[i].name, ticker: json.results[i].ticker, id: i});
-        }
-        setOptions(temp);
-      })
-        .catch(error => console.error(error))
-  },[]);
+    setLoading(true);
+    const fetchStocks = async () => {
+      setLoading(true);
+      fetch(currentPage)
+          .then(response => response.json())
+          .then(json => {
+            let temp: Array<Stock> = [];
+            for(let i = 0; i < stocksPerPage; i++){
+              temp.push({
+                name: json.results[i].name,
+                ticker: json.results[i].ticker,
+                currency: json.results[i].currency_name.toUpperCase(),
+                market: json.results[i].market.toUpperCase(),
+                exchange: json.results[i].primary_exchange.toUpperCase(),
+                id: json.results[i].ticker});
+            }
+            setOptions(temp);
+            history.push(json.next_url + `&apiKey=${process.env.SKsdbJqzL0Ehik47QA4ykN2Hh4Kk1PUO}`); // add next url
+            setHistory(history);
+          })
+          .catch(error => console.error(error))
+      setLoading(false);
+    };
+    fetchStocks();
+  }, []);
 
 
   return(
@@ -36,6 +60,9 @@ function Stocks() {
             <tr>
               <th>Name</th>
               <th>Ticker</th>
+              <th>Currency</th>
+              <th>Market</th>
+              <th>Exchange</th>
             </tr>
           </thead>
           <tbody>
@@ -44,7 +71,9 @@ function Stocks() {
                 <tr key={stock.id}>
                   <td>{stock.name}</td>
                   <td>{stock.ticker}</td>
-                  <td><button>Info</button></td>
+                  <td>{stock.currency}</td>
+                  <td>{stock.market}</td>
+                  <td>{stock.exchange}</td>
                 </tr>
             )
           })}
